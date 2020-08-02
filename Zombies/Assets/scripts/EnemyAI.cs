c044f7153a03bf88def467df59d3d1c7fe9b736c
GRANDE-I7\idela <i.delamo@gmail.com>
2020-08-02 15:52:52 +0200

    Chaseing range

--- Zombies/Assets/scripts/EnemyAI.cs
+++ Zombies/Assets/scripts/EnemyAI.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
@@ -7,17 +8,32 @@ public class EnemyAI : MonoBehaviour
 {
 
     [SerializeField] Transform target;
+    [SerializeField] float chaseRange = 5f;
+
     NavMeshAgent navMeshAgent;
-    // Start is called before the first frame update
+    
+    
+    float distanceToTarget = Mathf.Infinity;
     void Start()
     {
         navMeshAgent = GetComponent<NavMeshAgent>();
 
     }
 
-    // Update is called once per frame
+
     void Update()
     {
-        navMeshAgent.SetDestination(target.position);
+        distanceToTarget = Vector3.Distance(target.position, transform.position);
+        
+        if (distanceToTarget < chaseRange)
+        {
+            navMeshAgent.SetDestination(target.position);
+        }     else
+        {
+            print("Ya no te sigo");
+        }
+            
+     
+    
     }
 }
