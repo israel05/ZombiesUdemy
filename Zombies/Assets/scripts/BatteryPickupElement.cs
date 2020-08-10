1ad71028ed71119067979d25fa43ade79bfea466
GRANDE-I7\idela <i.delamo@gmail.com>
2020-08-11 00:28:55 +0200

    Lampara y batería

---
+++ Zombies/Assets/scripts/BatteryPickupElement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BatteryPickupElement : MonoBehaviour
+{
+    [SerializeField] float restoreAngle = 40f;
+    [SerializeField] float addIntensity = 5f;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            other.GetComponentInChildren<FlashLightSystem>().RestoreLightAngle(restoreAngle);
+            other.GetComponentInChildren<FlashLightSystem>().RestoreLightIntensity(addIntensity);
+            Destroy(gameObject);
+        }
+
+    }
+}
---
+++ Zombies/Assets/scripts/FlashLightSystem.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlashLightSystem : MonoBehaviour
+{
+    [SerializeField] float lightDecay = .1f;
+    [SerializeField] float angleDecay = 1f;
+    [SerializeField] float minimumAngle = 40f;
+
+    Light myLight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myLight = GetComponent<Light>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        DecreaseLightAngle();
+        DecreaseLightIntensity();
+
+    }
+
+
+    public void RestoreLightAngle( float resotreAngle)
+    {
+        myLight.spotAngle = resotreAngle;
+    }
+
+    public void RestoreLightIntensity(float intensityAmount)
+    {
+        myLight.intensity += intensityAmount;
+    }
+
+    private void DecreaseLightIntensity()
+    {
+        myLight.intensity -= lightDecay * Time.deltaTime;
+
+    }
+
+    private void DecreaseLightAngle()
+    {
+        if(myLight.spotAngle <= minimumAngle)
+        {
+            return;
+        }
+        else
+        {
+            myLight.spotAngle -= angleDecay * Time.deltaTime;
+        }
+    
+    }
+}
